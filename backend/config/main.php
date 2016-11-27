fb5e20790e81c672bc3f107b90381ee12733f2d8
Hugo Alejandro Giordano <hag8194@gmail.com>
2016-11-27 12:01:07 -0400

    Inital commit

---
+++ backend/config/main.php
@@ -0,0 +1,119 @@
+<?php
+$params = array_merge(
+    require(__DIR__ . '/../../common/config/params.php'),
+    require(__DIR__ . '/../../common/config/params-local.php'),
+    require(__DIR__ . '/params.php'),
+    require(__DIR__ . '/params-local.php')
+);
+
+return [
+    'id' => 'app-backend',
+    'basePath' => dirname(__DIR__),
+    'controllerNamespace' => 'backend\controllers',
+    'language'=>'es',
+    'sourceLanguage' => 'en-US',
+    'bootstrap' => ['log'],
+    'modules' => [
+        'admin' => [
+            'class' => 'mdm\admin\Module',
+            'controllerMap' => [
+                'assignment' => [
+                    'class' => 'mdm\admin\controllers\AssignmentController',
+                    'layout' => 'left-menu', // avaliable value 'left-menu', 'right-menu' and 'top-menu'
+                    'userClassName' => 'common\models\User',
+                    'idField' => 'id',
+                    'usernameField' => 'username',
+                    /*'fullnameField' => 'profile.full_name',
+                    'extraColumns' => [
+                        [
+                            'attribute' => 'full_name',
+                            'label' => 'Full Name',
+                            'value' => function($model, $key, $index, $column) {
+                                return $model->profile->full_name;
+                            },
+                        ],
+                        [
+                            'attribute' => 'dept_name',
+                            'label' => 'Department',
+                            'value' => function($model, $key, $index, $column) {
+                                return $model->profile->dept->name;
+                            },
+                        ],
+                        [
+                            'attribute' => 'post_name',
+                            'label' => 'Post',
+                            'value' => function($model, $key, $index, $column) {
+                                return $model->profile->post->name;
+                            },
+                        ],
+                    ],*/
+                    'searchClass' => 'common\models\searchmodels\UserSearch'
+                ],
+            ],
+            'menus' => [
+                'assignment' => [
+                    'label' => 'Grand Access' // change label
+                ],
+                'route' => null, // disable menu
+            ],
+        ]
+    ],
+    'components' => [
+        'request' => [
+            'csrfParam' => '_csrf-backend',
+        ],
+        'user' => [
+            'identityClass' => 'common\models\User',
+            'enableAutoLogin' => true,
+            'identityCookie' => ['name' => '_identity-backend', 'httpOnly' => true],
+        ],
+        'session' => [
+            // this is the name of the session cookie used for login on the backend
+            'name' => 'advanced-backend',
+        ],
+        'log' => [
+            'traceLevel' => YII_DEBUG ? 3 : 0,
+            'targets' => [
+                [
+                    'class' => 'yii\log\FileTarget',
+                    'levels' => ['error', 'warning'],
+                ],
+            ],
+        ],
+        'errorHandler' => [
+            'errorAction' => 'site/error',
+        ],
+        'assetManager' => [
+            'bundles' => [
+                'dmstr\web\AdminLteAsset' => [
+                    'skin' => 'skin-yellow-light',
+                ],
+            ],
+        ],
+        /*
+        'urlManager' => [
+            'enablePrettyUrl' => true,
+            'showScriptName' => false,
+            'rules' => [
+                '<controller:\w+>/<id:\d+>' => '<controller>/view',
+                '<controller:\w+>/<action:\w+>/<id:\d+>' => '<controller>/<action>',
+                '<controller:\w+>/<action:\w+>' => '<controller>/<action>'
+            ],
+        ],
+        */
+        'as access' => [
+            'class' => 'mdm\admin\components\AccessControl',
+            'allowActions' => [
+                'site/*',
+                'admin/*',
+                'some-controller/some-action',
+                // The actions listed here will be allowed to everyone including guests.
+                // So, 'admin/*' should not appear here in the production, of course.
+                // But in the earlier stages of your development, you may probably want to
+                // add a lot of actions here until you finally completed setting up rbac,
+                // otherwise you may not even take a first step.
+            ]
+        ]
+    ],
+    'params' => $params,
+];
---
+++ backend/views/site/example.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Hugo
+ * Date: 27/11/2016
+ * Time: 10:50 AM
+ */
+
+/* @var $this yii\web\View */
+
+$this->params['breadcrumbs'][] = 'About';
+
+$this->beginBlock('content-header'); ?>
+About <small>static page</small>
+<?php $this->endBlock(); ?>
+
+<div class="site-about">
+    <p> This is the About page. You may modify the following file to customize its content: </p>
+    <code><?= __FILE__ ?></code>
+</div>

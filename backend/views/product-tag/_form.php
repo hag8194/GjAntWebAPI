90a9ce1b531fad9bae87f1dc73c7eb963040d98a
feanoro <ers.cesar@gmail.com>
2017-01-22 17:36:51 -0400

    Etiquetas asociadas al producto no se vuelven a mostrar en el dropdownlist de etiquetas.

--- backend/views/product-tag/_form.php
+++ backend/views/product-tag/_form.php
@@ -5,6 +5,7 @@ use common\models\Tag;
 use yii\helpers\ArrayHelper;
 use yii\helpers\Html;
 use yii\widgets\ActiveForm;
+use yii\helpers\Url;
 
 /* @var $this yii\web\View */
 /* @var $model common\models\ProductTag */
@@ -15,9 +16,19 @@ use yii\widgets\ActiveForm;
 
     <?php $form = ActiveForm::begin(); ?>
 
-    <?= $form->field($model, 'product_id')->dropDownList(ArrayHelper::map(Product::find()->all(), 'id', 'name')) ?>
+    <?= $form->field($model, 'product_id')->dropDownList(ArrayHelper::map(Product::find()->all(), 'id', 'name'),
+        ['prompt'=>'Selecciona un Producto',
+            'class'=>'form-control',
+            'onchange'=>'
+             $.post("'.Yii::$app->urlManager->createUrl('product-tag/tags?id=').'"+$(this).val(),
+                function( data ){
+                   $( "select#tag" ).html( data ); 
+                });
+              ']);  ?>
 
-    <?= $form->field($model, 'tag_id')->dropDownList(ArrayHelper::map(Tag::find()->all(), 'id', 'name')) ?>
+    <?= $form->field($model, 'tag_id')->dropDownList(ArrayHelper::map(Tag::find()->all(), 'id', 'name'),['id'=>'tag',
+        'class'=>'form-control'
+    ]) ?>
 
     <div class="form-group">
         <?= Html::submitButton($model->isNewRecord ? Yii::t('backend', 'Create') : Yii::t('backend', 'Update'), ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
